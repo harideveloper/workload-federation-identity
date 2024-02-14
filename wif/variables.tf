4453bd1dfe2cd1151318053d313941fd065f680d
hariprasad-sundaresan-bjsscloud-net <hariprasad.sundaresan@bjsscloud.net>
2024-02-14 22:52:24 +0000

    WIF Test : Refactor & Cleanup

--- wif/variables.tf
+++ wif/variables.tf
@@ -1,29 +1,26 @@
 variable "project" {
   type        = string
   description = "GCP Prpject Name for Enablers Team"
-  default = "hariprasad-sundaresan-0202"
 }
 
 variable "region" {
   type = string
   description = "The primary region provision resources within."
-  default = "europe-west2"
 }
 
 variable "zone" {
   type = string
   description = "The primary zone to provision resources within."
-  default = "europe-west2-b"
 }
 
 variable "github_url" {
   type    = string
-  default = "https://github.com"
+  description = "Version Control URI e.g <https://github.com>"
 }
 
 variable "github_repo" {
   type    = string
-  default = "harideveloper/workload-federation-identity"
+  description = "Respective Version Control Repo e.g <harideveloper/workload-federation-identity>"
 }
 
 
