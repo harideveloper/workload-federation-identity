b1be0c857b49357fccde70c63a3ef235df7b2da3
hariprasad-sundaresan-bjsscloud-net <hariprasad.sundaresan@bjsscloud.net>
2024-02-14 21:49:54 +0000

    WIF Test : Initial Changes

--- resources/main.tf
+++ resources/main.tf
@@ -1,5 +1,5 @@
 
-resource "google_service_account" "workload_test_sa" {
-  account_id = "workload_test_sa"
+resource "google_service_account" "wif_resource" {
+  account_id = "wif-resource"
   display_name = "Terraform Resource created by Git Actions Using Workload Identity Auth"
 }
