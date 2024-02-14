e16f6b667914f3f37e36b1438a7a0293b2df68c7
hariprasad-sundaresan-bjsscloud-net <hariprasad.sundaresan@bjsscloud.net>
2024-02-14 22:57:50 +0000

    WIF Test : Refactor & Cleanup

--- resources/variables.tf
+++ resources/variables.tf
@@ -1,11 +1,9 @@
 variable "project" {
   type        = string
   description = "GCP Prpject Name for Enablers Team"
-  default = "hariprasad-sundaresan-0202"
 }
 
 variable "region" {
   type = string
   description = "The primary region provision resources within."
-  default = "europe-west2"
 }
